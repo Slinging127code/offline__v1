cf727f13dfd4013caf352652669efdd237c9d8f5
xinyi_jiao <2038310959@qq.com>
2025-03-25 21:30:20 +0800

    add 0325 coding  xinyi_jiao

---
+++ .idea/shell/2025325/quanbu.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+python3 /opt/soft/datax/job/2207A/xinyi_jiao/gen_import_config.py -d dev_realtime_v1_xinyu_luo -t activity_info
+python3 /opt/soft/datax/job/2207A/xinyi_jiao/gen_import_config.py -d dev_realtime_v1_xinyu_luo -t activity_order
+python3 /opt/soft/datax/job/2207A/xinyi_jiao/gen_import_config.py -d dev_realtime_v1_xinyu_luo -t activity_rule
+python3 /opt/soft/datax/job/2207A/xinyi_jiao/gen_import_config.py -d dev_realtime_v1_xinyu_luo -t activity_sku
+python3 /opt/soft/datax/job/2207A/xinyi_jiao/gen_import_config.py -d dev_realtime_v1_xinyu_luo -t base_category1
+python3 /opt/soft/datax/job/2207A/xinyi_jiao/gen_import_config.py -d dev_realtime_v1_xinyu_luo -t base_category2
+python3 /opt/soft/datax/job/2207A/xinyi_jiao/gen_import_config.py -d dev_realtime_v1_xinyu_luo -t base_category3
+python3 /opt/soft/datax/job/2207A/xinyi_jiao/gen_import_config.py -d dev_realtime_v1_xinyu_luo -t base_dic
+python3 /opt/soft/datax/job/2207A/xinyi_jiao/gen_import_config.py -d dev_realtime_v1_xinyu_luo -t base_province
+python3 /opt/soft/datax/job/2207A/xinyi_jiao/gen_import_config.py -d dev_realtime_v1_xinyu_luo -t base_region
+python3 /opt/soft/datax/job/2207A/xinyi_jiao/gen_import_config.py -d dev_realtime_v1_xinyu_luo -t base_trademark
+python3 /opt/soft/datax/job/2207A/xinyi_jiao/gen_import_config.py -d dev_realtime_v1_xinyu_luo -t cart_info
+python3 /opt/soft/datax/job/2207A/xinyi_jiao/gen_import_config.py -d dev_realtime_v1_xinyu_luo -t comment_info
+python3 /opt/soft/datax/job/2207A/xinyi_jiao/gen_import_config.py -d dev_realtime_v1_xinyu_luo -t coupon_info
+python3 /opt/soft/datax/job/2207A/xinyi_jiao/gen_import_config.py -d dev_realtime_v1_xinyu_luo -t coupon_use
+python3 /opt/soft/datax/job/2207A/xinyi_jiao/gen_import_config.py -d dev_realtime_v1_xinyu_luo -t date_info
+python3 /opt/soft/datax/job/2207A/xinyi_jiao/gen_import_config.py -d dev_realtime_v1_xinyu_luo -t favor_info
+python3 /opt/soft/datax/job/2207A/xinyi_jiao/gen_import_config.py -d dev_realtime_v1_xinyu_luo -t holiday_info
+python3 /opt/soft/datax/job/2207A/xinyi_jiao/gen_import_config.py -d dev_realtime_v1_xinyu_luo -t holiday_year
+python3 /opt/soft/datax/job/2207A/xinyi_jiao/gen_import_config.py -d dev_realtime_v1_xinyi_jiao -t order_detail
+python3 /opt/soft/datax/job/2207A/xinyi_jiao/gen_import_config.py -d dev_realtime_v1_xinyi_jiao -t order_info
+python3 /opt/soft/datax/job/2207A/xinyi_jiao/gen_import_config.py -d dev_realtime_v1_xinyi_jiao -t order_refund_info
+python3 /opt/soft/datax/job/2207A/xinyi_jiao/gen_import_config.py -d dev_realtime_v1_xinyi_jiao -t order_status_log
+python3 /opt/soft/datax/job/2207A/xinyi_jiao/gen_import_config.py -d dev_realtime_v1_xinyi_jiao -t payment_info
+python3 /opt/soft/datax/job/2207A/xinyi_jiao/gen_import_config.py -d dev_realtime_v1_xinyi_jiao -t sku_info
+python3 /opt/soft/datax/job/2207A/xinyi_jiao/gen_import_config.py -d dev_realtime_v1_xinyi_jiao -t spu_info
+python3 /opt/soft/datax/job/2207A/xinyi_jiao/gen_import_config.py -d dev_realtime_v1_xinyi_jiao -t user_info
+
